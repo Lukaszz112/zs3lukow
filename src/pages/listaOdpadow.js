10ae800f991014bd5b888b8300d66306dbf9ebe5
Lukaszz112 <93291505+Lukaszz112@users.noreply.github.com>
2021-12-14 00:04:43 +0100

    update

--- src/pages/listaOdpadow.js
+++ src/pages/listaOdpadow.js
@@ -22,12 +22,79 @@ class ListaOdpadow extends react.Component{
             };
 
         const title = "Lista odpadów";
+      
+        const trashList = [{"Item":"Puszki","trashType":"Metal"},{"Item":"Gazety","trashType":"Papier"},{"Item":"Przeterminowane tabletki","trashType":"Leki"},{"Item":"Zepsuta suszarka","trashType":"Elektrośmieć"},{"Item":"Butelka plastikowa","trashType":"Plastik"},{"Item":"Butelka szklana","trashType":"Szkło"},{"Item":"Stary telewizor","trashType":"Elektrośmieć"},{"Item":"Skórka od banana","trashType":"BIO"},{"Item":"Stary monitor","trashType":"Elektrośmieć"},{"Item":"Obierki z warzyw","trashType":"BIO"},{"Item":"Plastikowe reklamówki","trashType":"Plastik"}]
+        
+        
 
             return(
                 <>
                 <ListaWrapper>
-                    <h1>{title}</h1>
-                    <button onClick={onLoad}>eee</button>
+                    <h1>Strona w budowie</h1>
+                  <table id="trashListTable">
+      <tr>
+        <th>Odpad</th>
+        <th>Rodzaj</th>
+      </tr>
+      
+      <tr>
+        <td>
+          {trashList[0].Item}
+          </td>
+        <td>
+          {trashList[0].trashType}
+          </td>
+        </tr>
+      <tr>
+        <td>
+          {trashList[1].Item}
+          </td>
+        <td>
+          {trashList[1].trashType}
+          </td>
+        </tr>
+                    <tr>
+        <td>
+          {trashList[2].Item}
+          </td>
+        <td>
+          {trashList[2].trashType}
+          </td>
+        </tr>
+                    <tr>
+        <td>
+          {trashList[3].Item}
+          </td>
+        <td>
+          {trashList[3].trashType}
+          </td>
+        </tr>
+                    <tr>
+        <td>
+          {trashList[4].Item}
+          </td>
+        <td>
+          {trashList[4].trashType}
+          </td>
+        </tr>
+                    <tr>
+        <td>
+          {trashList[5].Item}
+          </td>
+        <td>
+          {trashList[5].trashType}
+          </td>
+        </tr>
+                    <tr>
+        <td>
+          {trashList[6].Item}
+          </td>
+        <td>
+          {trashList[6].trashType}
+          </td>
+        </tr>
+      
+    </table>
                 </ListaWrapper>
                 </>
             )
